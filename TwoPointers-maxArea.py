9a2ed9b795ae73c85f918f944873ca595ae92c2d
Ayeshaaa-Arshad <aishmalik037@gmail.com>
2024-04-14 00:18:53 +0500

    4/14/2024

--- TwoPointers-maxArea.py
+++ TwoPointers-maxArea.py
@@ -1,23 +1,18 @@
+from collections import Counter
 class Solution(object):
-    def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        left = 0
-        right = len(height)-1
-        maxArea = 0
+    def maxOperations(self, nums, k):
+        count = 0
+        num_freq=Counter(nums)
 
-        while left < right:
-            if height[left] < height[right]:
-                maxArea = max(maxArea, height[left] * (right - left))
-                left += 1
-            else:
-                maxArea = max(maxArea, height[right] * (right - left))
-                right -= 1
+        for num in nums:
+            complement = k - num
+            if complement in num_freq and num_freq[complement] > 0 and num_freq[num] > 0:
+                if num == complement and num_freq[num] < 2:
+                    continue
+                count += 1
+                num_freq[num] -= 1
+                num_freq[complement] -= 1
 
-        return maxArea
+        return count
 
 
-sol = Solution()
-print(sol.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
