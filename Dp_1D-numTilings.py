fce82c1411f4156b51c116faa91f87ae0a5b747e
Ayeshaaa-Arshad <aishmalik037@gmail.com>
2024-04-29 21:33:06 +0500

    4/29/2024

---
+++ Dp_1D-numTilings.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def numTilings(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 3:
+            return n
+
+        dp = [0] * (n + 1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 5
+
+        for i in range(4, n + 1):
+            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % (10**9 + 7)
+        return dp[-1]
+    
