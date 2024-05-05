17c91537d6294c552c49919d9f4c64691863c07a
Ayeshaaa-Arshad <aishmalik037@gmail.com>
2024-05-05 18:02:46 +0500

    5/5/2024

---
+++ LinkedList-pairSum.py
@@ -0,0 +1,18 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def pairSum(self, head):
+        values = []
+        while head:
+            values.append(head.val)
+            head = head.next
+
+        max_sum = 0
+        n = len(values)
+        for i in range(n // 2):
+            max_sum = max(max_sum, values[i] + values[n - 1 - i])
+
+        return max_sum
